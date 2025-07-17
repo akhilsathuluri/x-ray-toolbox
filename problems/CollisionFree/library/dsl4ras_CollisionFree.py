394ab609a1c03fb42cd3da0da7db8c1892153046
asathuluri <akhil.sathuluri@tum.de>
2025-07-17 12:47:20 +0200

    diva 114v int done

--- problems/CollisionFree/library/dsl4ras_CollisionFree.py
+++ problems/CollisionFree/library/dsl4ras_CollisionFree.py
@@ -1,14 +1,14 @@
 import numpy as np
 from pydrake.all import *
 import sys
+from joblib import Parallel, delayed
 from pathlib import Path
-
 sys.path.append(
-    (Path(__file__).parent.parent.parent.parent.parent / "DIVA-R1.1.4-integrated")
+    (Path(__file__).parent.parent.parent.parent.parent / "dsl4ras-pfd")
     .resolve()
     .__str__()
 )
-from diva_collision_free import DIVAR114IntSystem
+from SCARA_collision_free import ScaraSystem
 
 
 class CollisionFree:
@@ -23,20 +23,28 @@ class CollisionFree:
                 [2, 3],
                 [4, 5],
                 [6, 7],
-                [8, 9],
             ]
         )
-        self.sim_system = DIVAR114IntSystem()
-        self.sim_system.setup_robots_scene()
+        num_robots = 4
+        self.sim_system = ScaraSystem(num_robots=num_robots)
+        nominalDV = np.array([0.8, 1.5, 1000, 0.9, 650, 160, 0.9, 10, 0.39, 0.26])
+        dvs = np.tile(nominalDV, (num_robots, 1))
+        self.sim_system.setup_robots_scene(dvs=dvs, modify_urdf=False)    
 
     def _compute_commons(self, dv_samples):
         self.var = dv_samples
+        # compute collision for the chosen angles
         var_list = self.var.values.tolist()
+        # self.qoi_values = np.array(
+        #     Parallel(n_jobs=-1)(
+        #         delayed(self.sim_system.compute_collision_in_config)(var) for var in var_list
+        #     ),
+        #     dtype=int)
         self.qoi_values = []
         for var in var_list:
             qoi = self.sim_system.compute_collision_in_config(var)
             self.qoi_values.append(qoi)
         self.qoi_values = np.array(self.qoi_values, dtype=int)
-
+    
     def d_min(self):
         self.var["d_min"] = self.qoi_values
